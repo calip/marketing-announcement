9c1dc7a0eb345e9c06ca520e03e06b0088843d10
alip putra <asalip.putra@gmail.com>
2022-08-03 16:30:03 +0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Marketing Announcement
+      </header>
+    </div>
+  );
+}
+
+export default App;
