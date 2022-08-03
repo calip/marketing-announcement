69a139ed8750ec1880ed83a42e162b1caf94d33a
alip putra <asalip.putra@gmail.com>
2022-08-03 18:24:37 +0700

    adding inline logic function

--- src/App.js
+++ src/App.js
@@ -1,11 +1,82 @@
+
+import React from 'react';
 import './App.css';
 
+async function searchItem(q) {
+  q = encodeURIComponent(q);
+  const response = await fetch(`https://marketing-announcement-api.vercel.app/api/marketing-announcement`, {
+    "method": "GET",
+  });
+  const body = await response.json();
+  console.log(q, body)
+  return body;
+}
 function App() {
+  const [query, setQuery] = React.useState("");
+  const [list, setList] = React.useState(null);
+
+  const search = (e) => {
+    e.preventDefault();
+    searchItem(query).then(setList);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        Marketing Announcement
-      </header>
+      <form onSubmit={search}>
+        <input
+          autoFocus
+          value={query}
+          onChange={e => setQuery(e.target.value)}
+        />
+        <button>Search</button>
+      </form>
+
+      {!list
+        ? null
+        : list.length === 0
+          ? <p><i>No results</i></p>
+          : <div className="item-container">
+            {list.map((item, i) => (
+              <Item key={i} item={item} />
+            ))}
+          </div>
+      }
+    </div>
+  );
+}
+
+function Item({ item }) {
+  const formatDate = s => new Date(s).toLocaleDateString(undefined, { dateStyle: 'long' });
+
+  return (
+    <div className="item">
+
+      {item.thumbnail &&
+          <img
+            alt=""
+            src={item.thumbnail}
+            className="responsive"
+          />
+      }
+
+      <div className="right">
+        <h2 className="title">
+          <a href={item.title}>{item.title}</a>
+        </h2>
+
+        <p className="description">
+          {item.description}
+        </p>
+
+        <div className="meta">
+          <span>{formatDate(item.updatedOn)}</span>
+
+          <span className="provider">
+            {item.updatedBy}
+          </span>
+        </div>
+      </div>
+
     </div>
   );
 }
