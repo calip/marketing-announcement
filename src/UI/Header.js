1d3deb2afa85f723a876ee3a3eb6f1bb646f1ffe
alip putra <asalip.putra@gmail.com>
2022-08-04 14:00:18 +0700

    add header component

---
+++ src/UI/Header.js
@@ -0,0 +1,19 @@
+const Header = () => {
+
+return(
+  <nav>
+    <ul className="nav-menu">
+      <li>
+        <a href="#">
+          <img src="https://picsum.photos/id/98/200" className="nav-image" alt="Profile" />
+          John
+        </a>
+      </li>
+    </ul>
+  </nav>
+)
+
+
+}
+
+export default Header
