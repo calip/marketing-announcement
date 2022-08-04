d3e8e400a960c0b1b6ec36fceaf207e286697b48
alip putra <asalip.putra@gmail.com>
2022-08-04 14:17:12 +0700

    update header UI

--- src/UI/Header.js
+++ src/UI/Header.js
@@ -4,9 +4,9 @@ return(
   <nav>
     <ul className="nav-menu">
       <li>
-        <a href="#">
-          <img src="https://picsum.photos/id/98/200" className="nav-image" alt="Profile" />
-          John
+        <a href="/#">
+          <img src="https://picsum.photos/id/98/200" className="nav-image" alt="Company A" />
+          Company A
         </a>
       </li>
     </ul>
